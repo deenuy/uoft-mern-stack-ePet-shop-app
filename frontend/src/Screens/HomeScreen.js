ffad6771ee2134b3119dbdff2ebdc23249e7f094
deenuy <deenuy@gmail.com>
2020-10-31 13:11:57 -0400

    UI/Ux enhancements on Home sreen

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -44,7 +44,7 @@ function HomeScreen (props){
                           <Link to={"/product/" + product._id}>{product.name}</Link>
                       </div>
                       <div className="product-brand">{product.brand}</div>
-                      <div className="product-price">{product.price}</div>
+                      <div className="product-price">${product.price}</div>
                       <div className="product-rating">{product.rating} Stars ({product.numReviews})</div>
                   </div>
                 </li>
