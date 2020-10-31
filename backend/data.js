58c18b3e8641b629f976496953004f2a9b6bd052
PH - ACER <henriqueph@hotmail.com>
2020-10-31 11:43:07 -0300

    Adding sample data, and connection to mongodb

--- backend/data.js
+++ backend/data.js
@@ -1,103 +1,113 @@
 export default {
-    products: [{
-        _id: '1',
-        name: "WholeHearted Grain Free All Life Stages Beef & Pea Formula Dry Dog Food, 25 lbs.",
-        category: 'Food',
-        image: '/imgs/img-01.jpg',
-        price: 34.99,
-        brand: 'WholeHearted',
-        rating: 4.75,
-        numReviews: 1676
-    },
-    {
-        _id: '2',
-        name: "Greenies Blueberry Flavor Regular Size Dog Dental Chews, 12 oz.",
-        category: 'Food',
-        image: '/imgs/img-02.jpg',
-        price: 18.98,
-        brand: 'Greenies',
-        rating: 5,
-        numReviews: 450
-    },
-    {
-        _id: '3',
-        name: "Blue Buffalo Life Protection Formula Natural Senior Chicken and Brown Rice Dry Dog Food, 30 lbs.",
-        category: 'Food',
-        image: '/imgs/img-03.jpg',
-        price: 69.98,
-        brand: 'Blue Buffalo',
-        rating: 4.25,
-        numReviews: 430
-    },
-    {
-        _id: '4',
-        name: "Blue Buffalo Wilderness Natural Senior High Protein Grain Free Chicken Dry Dog Food, 24 lbs",
-        category: 'Food',
-        image: '/imgs/img-04.jpg',
-        price: 43.58,
-        brand: 'Blue Buffalo',
-        rating: 3.25,
-        numReviews: 125
-    },
-    {
-        _id: '5',
-        name: "KONG Wubba Weave Dog Toy, Small",
-        category: 'Toy',
-        image: '/imgs/img-05.jpg',
-        price: 8.55,
-        brand: 'Kong',
-        rating: 3.75,
-        numReviews: 43
-    },
-    {
-        _id: '6',
-        name: "KONG Wubba Finz Blue Toy for Dog, Small",
-        category: 'Toy',
-        image: '/imgs/img-06.jpg',
-        price: 6.99,
-        brand: 'Kong',
-        rating: 4.5,
-        numReviews: 22
-    },
-    {
-        _id: '7',
-        name: "KONG SoftSeas Octopus Dog Toy, Small",
-        category: 'Toy',
-        image: '/imgs/img-07.jpg',
-        price: 8.49,
-        brand: 'Kong',
-        rating: 4.0,
-        numReviews: 60
-    },
-    {
-        _id: '8',
-        name: "KONG SoftSeas Turtle Dog Toy, Small",
-        category: 'Toy',
-        image: '/imgs/img-08.jpg',
-        price: 3.25,
-        brand: 'Kong',
-        rating: 2,
-        numReviews: 4
-    },
-    {
-        _id: '9',
-        name: "Wellness Natural Grain Free Lamb & Salmon Recipe Soft Puppy Bites Dog Treats, 3 oz",
-        category: 'Food',
-        image: '/imgs/img-09.jpg',
-        price: 4.29,
-        brand: 'Wellness',
-        rating: 3.5,
-        numReviews: 283
-    },
-    {
-        _id: '10',
-        name: "Nylabone Blue Bone Chicken Puppy Chew, X-Small",
-        category: 'Toy',
-        image: '/imgs/img-10.jpg',
-        price: 1.99,
-        brand: 'Wellness',
-        rating: 4.5,
-        numReviews: 83
-    }
-]
+  products: [{
+    "name": "WholeHearted Grain",
+    "description": "WholeHearted Grain Free All Life Stages Beef & Pea Formula Dry Dog Food, 25 lbs.",
+    "category": "Food",
+    "image": "/uploads/img-01.jpg",
+    "price": 34.99,
+    "brand": "WholeHearted",
+    "rating": 4.75,
+    "numReviews": 1676,
+    "countInStock": 10
+  },
+  {
+    "name": "Greenies Blueberry Flavor",
+    "description": "Greenies Blueberry Flavor Regular Size Dog Dental Chews, 12 oz.",
+    "category": "Food",
+    "image": "/uploads/img-02.jpg",
+    "price": 18.98,
+    "brand": "Greenies",
+    "rating": 5,
+    "numReviews": 450,
+    "countInStock": 13
+  },
+  {
+    "name": "Blue Buffalo Life Protection",
+    "description": "Blue Buffalo Life Protection Formula Natural Senior Chicken and Brown Rice Dry Dog Food, 30 lbs.",
+    "category": "Food",
+    "image": "/uploads/img-03.jpg",
+    "price": 69.98,
+    "brand": "Blue Buffalo",
+    "rating": 4.25,
+    "numReviews": 430,
+    "countInStock": 50
+  },
+  {
+    "name": "Blue Buffalo Wilderness",
+    "description": "Blue Buffalo Wilderness Natural Senior High Protein Grain Free Chicken Dry Dog Food, 24 lbs",
+    "category": "Food",
+    "image": "/uploads/img-04.jpg",
+    "price": 43.58,
+    "brand": "Blue Buffalo",
+    "rating": 3.25,
+    "numReviews": 125,
+    "countInStock": 110
+  },
+  {
+    "name": "KONG Wubba Weave",
+    "description": "KONG Wubba Weave Dog Toy, Small",
+    "category": "Toy",
+    "image": "/uploads/img-05.jpg",
+    "price": 8.55,
+    "brand": "Kong",
+    "rating": 3.75,
+    "numReviews": 43,
+    "countInStock": 234
+  },
+  {
+    "name": "KONG Wubba Finz",
+    "description": "KONG Wubba Finz Blue Toy for Dog, Small",
+    "category": "Toy",
+    "image": "/uploads/img-06.jpg",
+    "price": 6.99,
+    "brand": "Kong",
+    "rating": 4.5,
+    "numReviews": 22,
+    "countInStock": 71
+  },
+  {
+    "name": "KONG SoftSeas Octopus",
+    "description": "KONG SoftSeas Octopus Dog Toy, Small",
+    "category": "Toy",
+    "image": "/uploads/img-07.jpg",
+    "price": 8.49,
+    "brand": "Kong",
+    "rating": 4.0,
+    "numReviews": 60,
+    "countInStock": 97
+  },
+  {
+    "name": "KONG SoftSeas Turtle",
+    "description": "KONG SoftSeas Turtle Dog Toy, Small",
+    "category": "Toy",
+    "image": "/uploads/img-08.jpg",
+    "price": 3.25,
+    "brand": "Kong",
+    "rating": 2,
+    "numReviews": 4,
+    "countInStock": 85
+  },
+  {
+    "name": "Wellness Natural Grain",
+    "description": "Wellness Natural Grain Free Lamb & Salmon Recipe Soft Puppy Bites Dog Treats, 3 oz",
+    "category": "Food",
+    "image": "/uploads/img-09.jpg",
+    "price": 4.29,
+    "brand": "Wellness",
+    "rating": 3.5,
+    "numReviews": 283,
+    "countInStock": 49
+  },
+  {
+    "name": "Nylabone Blue Bone",
+    "description": "Nylabone Blue Bone Chicken Puppy Chew, X-Small",
+    "category": "Toy",
+    "image": "/uploads/img-10.jpg",
+    "price": 1.99,
+    "brand": "Wellness",
+    "rating": 4.5,
+    "numReviews": 83,
+    "countInStock": 18
+  }
+  ]
 }
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,34 @@
 import express from 'express';
-import data from "./data";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+import productRouter from './routers/productRouter.js';
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/products", (req, res) => {
-    res.send(data.products);
-})
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/e-petshot', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-app.listen(5000, ()=> {console.log("Server started at http://localhost:5000")})
+app.use('/api/products', productRouter);
+app.use('/', productRouter);
+app.use('/seed', productRouter);
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
+
