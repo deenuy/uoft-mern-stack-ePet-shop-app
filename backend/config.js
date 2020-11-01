c3dbbccf83087b0e842e8cf37c0fc08c5500ffba
PH - ACER <henriqueph@hotmail.com>
2020-11-01 09:37:29 -0300

    Backend files (server.js, config  and routes)

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  PORT: process.env.PORT || 5000,
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/e-petshop',
+  JWT_SECRET: process.env.JWT_SECRET || 'e-petshop2020#',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+  accessKeyId: process.env.accessKeyId || 'accessKeyId',
+  secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
+};
