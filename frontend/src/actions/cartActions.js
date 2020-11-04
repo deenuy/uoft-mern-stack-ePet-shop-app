ba8d42a8c8dc6f8eac29738c322afe8be66f4e1f
Stefan Salituro <stefansalituro@gmail.com>
2020-11-04 10:25:21 -0500

    stefan's recent edits to app.js, CartScreen.js, index.js, store.js, cartActions.js, cartReducers.js.

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,17 +1,33 @@
-const addToCart(productId, qty) => async (dispatch) =>{
+import Axios from "axios";
+import Cookie from "js-cookie";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING, CART_SAVE_PAYMENT } from "../constants/cartConstants";
 
+const addToCart = (productId, qty) => async (dispatch, getState) => {
     try {
       const {data} = await Axios.get("/api/products/" + productId);
-      dispatch{type: CART_ADD_ITEM, payload:{
+      dispatch({
+          type: CART_ADD_ITEM, payload: {
           product: data._id, 
           name: data.name,
-          name: data.name, 
-          name: data.name, 
-          name: data.name,
-
-      }};
+          image: data.image, 
+          price: data.price, 
+          countInStock: data.countInStock,
+          qty
+      }
+    }};
+    const { cart: { cartItems } } = getState();
+    Cookie.set("cartItems", JSON.stringify(cartItems));
 
     } catch  (error) {
 
     }
 }
+
+const removeFromCart = (productId) => (dispatch, getState) => {
+    dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+  
+    const { cart: { cartItems } } = getState();
+    Cookie.set("cartItems", JSON.stringify(cartItems));
+  }
+
+  export { addToCart, removeFromCart, saveShipping, savePayment }
