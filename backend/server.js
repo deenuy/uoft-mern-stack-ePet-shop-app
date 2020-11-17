8dc2745a6b82f3303bfa414f2998287364923c15
deenuy <deenuy@gmail.com>
2020-11-16 21:10:29 -0500

    added babel compiler lines in server.js and removed nodemon from package.json as heroku does not contains the library

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,12 @@ app.use('/api/products', productRoute);
 app.use('/api/orders', orderRoute);
 app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
 
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+});
+
 app.listen(config.PORT, () => {
   console.log('Server started at http://localhost:5000');
 });
